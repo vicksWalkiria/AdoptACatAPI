450910a62b770a0f784b908119a588a22e9e3c3c
vicksWalkiria <vicks630@gmail.com>
2020-02-27 22:51:56 +0100

    add get cat service for cat activity

--- api/v1/get_cat.php
+++ api/v1/get_cat.php
@@ -8,25 +8,22 @@ header("Access-Control-Allow-Methods: GET");
 
 $user_id = $_GET['user_id'];
 $token = $_GET['token'];
-$adoption = $_GET['adoption'];
+$id = $_GET['cat_id'];
 
 $db= new db();
-if(strlen($user_id) == 0 || strlen($token) == 0 || strlen($adoption) == 0|| !$db->isTokenValid($user_id, $token))
+if(strlen($user_id) == 0 || strlen($token) == 0 || strlen($id) == 0|| !$db->isTokenValid($user_id, $token))
 {
  	$response = array("code" => "400", "data" => "You must provide valid params");
- 	if(!$db->isTokenValid($user_id, $token))
- 	{
- 		$response = array("code" => "400", "data" => "Invalid token");
- 	}
 }
 else
 {
-	$sql = 'SELECT * FROM cats WHERE adoption = '.$adoption;
+	$sql = 'SELECT * FROM cats WHERE id = '.$id;
 	$db= new db();
 	$database = $db->connectionDB();
 	$result = $database->query($sql);
 	$cats = $result ->fetchAll(PDO::FETCH_OBJ);
-	$response = array("code" => "200", "data" => array("cats" =>$cats, "token" => $db->generateToken($user_id))); 
+	$cats = $cats[0];
+	$response = array("code" => "200", "data" => array("cat" =>$cats, "token" => $db->generateToken($user_id))); 
 }
 
 $db = null;
